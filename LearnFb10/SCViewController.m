e644b66fe3e58d5a6774f1d59f452335f254c32c
Lav Tomar <lav@Rajesh-YML.local>
2013-08-14 10:47:37 +0530

    Initial Commit

---
+++ LearnFb10/SCViewController.m
@@ -0,0 +1,29 @@
+//
+//  SCViewController.m
+//  LearnFb10
+//
+//  Created by Lav Tomar on 14/08/13.
+//  Copyright (c) 2013 YML. All rights reserved.
+//
+
+#import "SCViewController.h"
+
+@interface SCViewController ()
+
+@end
+
+@implementation SCViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ LearnFb10/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LearnFb10
+//
+//  Created by Lav Tomar on 14/08/13.
+//  Copyright (c) 2013 YML. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SCAppDelegate class]));
+    }
+}
